655f17244a34484828b5fc80cdfc64edced3b326
Vëx <veljko.tekelerovic@gmail.com>
2024-08-21 21:07:17 +0200

    Updated About page (initially)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+//TODO: Add styling later
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+        <h1>Infoportal</h1>
+        <h3>Infoportal predstavlja jednostavan način za povezivanje ljudi jedne zajednice</h3>
+        
+        <div>
+          <p>Cilj platforme je da ljudima omogući brz, pouzdan i jednostavan način za iskazivanje:</p>
+          <ul>
+            <li>pitanja</li>
+            <li>planova</li>
+            <li>ciljeva</li>
+            <li>društveno korisnih aktivnosti</li>
+            <li>i ostalih stvari korisnih za jednu zajednicu.</li>
+          </ul>
+        </div>
+
+        <p>
+          Svaki korisnik ima mogućnost ne ometane upotrebe platforme.
+        </p>
+        <p>
+          Korisnici će poštovati osnovna pravila upotrebe platforme definisane u dokumentu.
+        </p>
+    </>
   )
 }
 
