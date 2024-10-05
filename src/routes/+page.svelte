141707154dd08463bb16fe376be30a6cd35735b8
Vëx <veljko.tekelerovic@gmail.com>
2024-10-05 19:04:35 +0200

    Moved authentication logic to server action

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,28 +6,23 @@
     export let data;
 
     async function handleGoogleLogin(response) {
-        const { credential } = response;
-        console.debug("Google login response received.");
-
-        // perform signup via google
-        const authResponse = await data.supaAuth.signInWithIdToken({
-            provider: 'google',
-            token: credential
-        })
-        //TODO: add NONCE
-
-        // check for errors
-        if(authResponse.error) {
+        // check if there's any response
+        if(!response) {
+            // show error dialogs
             console.error("Unable to login user with Google");
-            console.error(authResponse.error);
             showDialog(true);
         }
+        
+        // extract the response and initiate
+        const { credential } = response;
+        console.debug("Google login response received, redirecting...");
 
-        // display session data
-        console.debug("Registered user ID: ", authResponse.data.user?.id);
-        console.debug("Redirecting to list");
-        // throw redirect(301, '/list')
-        goto('/list')
+        const resp = await fetch(`/?auth=${credential}`);
+        if(resp.redirected) {
+            console.debug("Redirecting to: ", resp.url);
+            // console.log("Login completed...");
+            await goto(resp.url);
+        }
     }
 
     onMount(async () => {
@@ -69,25 +64,8 @@
     <p>100 људи, 100 ћуди</p>
 
     <div id="googleSignInButton"></div>
-    <!-- <div id="g_id_onload"
-        data-client_id="618627880154-3v3ojejgm2que1eijc2msq8jrvbcqpsm.apps.googleusercontent.com"
-        data-context="use"
-        data-ux_mode="popup"
-        data-callback="handleGoogleLogin"
-        data-nonce=""
-        data-auto_select="true"
-        data-itp_support="true"
-        data-use_fedcm_for_prompt="true">
-    </div>
 
-    <div class="g_id_signin"
-        data-type="standard"
-        data-shape="rectangular"
-        data-theme="outline"
-        data-text="continue_with"
-        data-size="large"
-        data-logo_alignment="left">
-    </div> -->
+    <!-- <button on:click={handleGoogleLogin}>test</button> -->
 </main>
 
 <style>
---
+++ src/routes/+server.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from './$types';
+import { redirect } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ locals: { supabase }, url }) => {
+    // check if we received any credentials
+    const authCredentials = url.searchParams.get('auth');
+    if(authCredentials) {
+        // perform signup via google
+        const authResponse = await supabase.auth.signInWithIdToken({
+            provider: 'google',
+            token: authCredentials
+        })
+        //TODO: add NONCE
+
+        // check for errors
+        if(authResponse.error) {
+            //TODO: Redirect to static page /auth-error
+            console.error("Signup error. Will redirect to /auth-error")
+            // throw redirect(301, '/auth-error');
+        }
+        
+        console.debug("User successfully registered.")
+        throw redirect(301, '/list');
+    } else {
+        return new Response();  //pass through
+    }
+};
