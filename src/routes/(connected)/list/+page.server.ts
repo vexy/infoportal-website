cedb5fb2e99f04e4fd1b2d437315d722fbbae033
Vëx <veljko.tekelerovic@gmail.com>
2024-10-05 22:13:02 +0200

    Improved access to protected routes

--- src/routes/(connected)/list/+page.server.ts
+++ src/routes/(connected)/list/+page.server.ts
@@ -1,7 +1,9 @@
 import { QuestionService } from '$lib/QuestionsService';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ( { locals: { supabase } }) => {
+export const load = (async ( { locals: { supabase }, parent }) => {
+    await parent();
+
     // get all the questions of the system
     const qService = new QuestionService(supabase);
     const allQuestions = await qService.fetchAllQuestions();
