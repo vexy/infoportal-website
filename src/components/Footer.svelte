84fea9b9437ab12a6977ec8f98d393623cc9841d
Vëx <veljko.tekelerovic@gmail.com>
2024-10-27 21:21:01 +0100

    Added Privacy Policy and Cookie Policy
    Closes #43

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,7 +8,7 @@
     </a>
     <a href="/learn-more">Сазнај више</a>
     <a href="/support-us">Подржи развој</a>
-    <a href="/learn-more">Политика приватности података</a>
-    <a href="/learn-more">Политика колачића</a>
+    <a href="https://www.iubenda.com/privacy-policy/10283915" class="iubenda-black iubenda-noiframe iubenda-embed iubenda-noiframe " title="Политика приватности">Политика приватности</a>
+    <a href="https://www.iubenda.com/privacy-policy/10283915/cookie-policy" class="iubenda-black iubenda-noiframe iubenda-embed iubenda-noiframe " title="Политика колачића">Политика колачића</a>
     <a href="https://github.com/vexy/infoportal-website" target="_blank">Кôд платформе</a>
 </footer>
